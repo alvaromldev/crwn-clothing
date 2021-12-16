22d90c1df0f4c396dc54e085087418dc6137a1a9
AlvaroM <alvaro.martin.lopez@met.ey.com>
2021-12-16 15:09:37 +0100

    updated shop data with new data, updated cart utils to group items in cartReducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { CollectionItem } from '../collection-item/collection-item.component';
+import CollectionItem from '../collection-item/collection-item.component';
 
 export const CollectionPreview = ({ title, items }) => (
-  <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
-      {items.slice(0, 4).map(({ id, ...otherItemProps }) => (
-        <CollectionItem key={id} {...otherItemProps} />
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items.slice(0, 4).map((item) => (
+        <CollectionItem key={item.id} item={item} />
       ))}
     </div>
   </div>
