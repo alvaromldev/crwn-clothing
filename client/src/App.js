1cc0222de7edc0cfd9eb89c8e4cddf1ff5b126b5
AlvaroM <alvaro.martin.lopez@met.ey.com>
2022-01-11 15:05:34 +0100

    added performance optimizations and error and loading handlers

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
 import Header from './components/header/header.component';
+import Spinner from './components/spinner/spinner.component';
 import { GlobalStyle } from './global.styles';
-import CheckoutPage from './pages/checkout/checkout.component';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { checkUserSession } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const ShopPage = lazy(() => import('./pages/shop/shop.component'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
+const SignInAndSignUpPage = lazy(() =>
+  import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component')
+);
 
 const App = () => {
   const currentUser = useSelector(selectCurrentUser);
@@ -22,21 +27,25 @@ const App = () => {
     <div>
       <GlobalStyle />
       <Header />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/shop/*' element={<ShopPage />} />
-        <Route path='/checkout' element={<CheckoutPage />} />
-        <Route
-          path='/signin'
-          element={
-            !!currentUser ? (
-              <Navigate to='/' replace />
-            ) : (
-              <SignInAndSignUpPage />
-            )
-          }
-        />
-      </Routes>
+      <ErrorBoundary>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/shop/*' element={<ShopPage />} />
+            <Route path='/checkout' element={<CheckoutPage />} />
+            <Route
+              path='/signin'
+              element={
+                !!currentUser ? (
+                  <Navigate to='/' replace />
+                ) : (
+                  <SignInAndSignUpPage />
+                )
+              }
+            />
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 };
